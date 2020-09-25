d834a70a5dc6a6b0a02837cb40f3b11f08f8d9a2
Jordan Pierce <jordanpierce67@tamu.edu>
2020-09-25 12:43:26 -0500

    Update panel_image.py

--- panel_image.py
+++ panel_image.py
@@ -18,28 +18,29 @@ def crop_center_by(image, zoom):
     return image[x - either_side : x + either_side, y - either_side : y + either_side]
 
 
-# Image coming is expected to be 112, 112;
-# for each iteration, the image is center-cropped so each
+# For each iteration, the image is center-cropped so each
 # successive image is *zoomed* in on, and resized to 112, 112.
 def panel(img):
+
+    panel_img = np.zeros([224, 224, 3])
 
     # Values between .5 and .95 are worth trying
     zoom = .72
 
-    panel_img = np.zeros([224, 224, 3])                                             # Panel image to be made
+    scale_1 = img                                                                   # 224 by 224 patch
+    scale_2 = scale_1                                                               # 224 by 224 patch
+    scale_1 = skimage.transform.resize(scale_1, (112, 112), anti_aliasing = True)   # Taking ^ that ^ image and downsizing to 112 
     
-    scale_1 = img                                                                   # Scale 1, original size 
-                                                                                    
-    scale_2 = crop_center_by(scale_1, zoom)                                         # Taking Scale 1, cropping off excess zoom% 
+    scale_2 = crop_center_by(scale_2, zoom)                                         # From the copy, cropping off excess % (w, h)
     scale_3 = scale_2                                                               # Making a copy of ^ this ^
-    scale_2 = skimage.transform.resize(scale_2, (112, 112), anti_aliasing = True)   # Resizing Scale 2 to 112, 112
+    scale_2 = skimage.transform.resize(scale_2, (112, 112), anti_aliasing = True)   # Downsizing the cropped zoomed copy to 112 by 112
     
-    scale_3 = crop_center_by(scale_3, zoom)                                         # Taking Scale 2 and cropping off excess zoom%
+    scale_3 = crop_center_by(scale_3, zoom)                                         # From the copy of the zoomed copy, cropping off excess % (w, h)
     scale_4 = scale_3                                                               # Making a copy of ^ this ^
-    scale_3 = skimage.transform.resize(scale_3, (112, 112), anti_aliasing = True)   # Resizing Scale 3 to 112, 112
+    scale_3 = skimage.transform.resize(scale_3, (112, 112), anti_aliasing = True)   # Downsizing the cropped zoomed copy of the zoomed copy to 112 by 112
 
-    scale_4 = crop_center_by(scale_4, zoom)                                         # Taking Scale 3 and cropping off excess zoom%
-    scale_4 = skimage.transform.resize(scale_4, (112, 112), anti_aliasing = True)   # Resizing Scale 4 to 112, 112
+    scale_4 = crop_center_by(scale_4, zoom)                                         # From the zoomed copy of the zoomed copy, cropping of excess % (w, h)
+    scale_4 = skimage.transform.resize(scale_4, (112, 112), anti_aliasing = True)   # Downsizing the cropped zoomed copy of the zoomed copy of the zoomed copy 112 by 112 :)
     
     panel_img[0:112, 0:112, :] = scale_1
     panel_img[0:112, 112:224, :] = scale_2
@@ -48,4 +49,4 @@ def panel(img):
 
     del img, scale_1, scale_2, scale_3, scale_4
     
-    return panel_img                                                               # Image returned is a 4-panel image, (224, 224)
+    return panel_img                                                                 # Image returned is a 4-panel image, (224, 224), [0, 1]
